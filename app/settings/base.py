b188e915938e24884e902a34df5e519ed5616274
Armestrong <Armestrongcalazans@hotmail.com>
2021-05-03 13:58:19 -0300

    Edit

--- app/settings/base.py
+++ app/settings/base.py
@@ -124,8 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'vol/web/static'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'vol/web/static/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
