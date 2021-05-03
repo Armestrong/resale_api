5d5c69294648f9b755ca3dc279a3f5d5ecf50913
Armestrong <Armestrongcalazans@hotmail.com>
2021-05-03 13:52:54 -0300

    Edit

--- app/settings/base.py
+++ app/settings/base.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/var/www/staticfiles'
+STATIC_ROOT = 'vol/web/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
